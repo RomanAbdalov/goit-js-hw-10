a87ab8a53e152398e7c4e03a351e91df6ca84793
RomanAbdalov <117114038+RomanAbdalov@users.noreply.github.com>
2023-04-13 19:22:26 +0300

    add start files task 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.getElementById('search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+inputEl.addEventListener('input', (event) => {
+   const inputTerm = event.targer.value.trim();
+
+if( inputTerm === '') {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+    return;
+}
+
+});
+
+
+
+
+
+
+
+
