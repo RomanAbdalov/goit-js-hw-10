1591342532f852976e356c4fccdee20b35a20f51
RomanAbdalov <117114038+RomanAbdalov@users.noreply.github.com>
2023-04-13 16:22:53 +0000

    Deploying to gh-pages from @ RomanAbdalov/goit-js-hw-10@a87ab8a53e152398e7c4e03a351e91df6ca84793 🚀

---
+++ index.008aab5e.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("search-box"),n=document.querySelector(".country-list"),t=document.querySelector(".country-info");e.addEventListener("input",(function(e){if(""===e.targer.value.trim())return n.innerHTML="",void(t.innerHTML="")}))}();
+//# sourceMappingURL=index.008aab5e.js.map
