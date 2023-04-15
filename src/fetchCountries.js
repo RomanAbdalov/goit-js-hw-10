e4645d77e0207f4c44c86016eb04fdacb6417b53
RomanAbdalov <117114038+RomanAbdalov@users.noreply.github.com>
2023-04-15 17:22:52 +0300

    fetch parcel files, add task files

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,12 @@
-export function fetchCountries(name){
-    return fetch(`https://restcountries.com/v3.1/name/{name}`).then((response) => response.json())
-    
-}
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const url = 'https://restcountries.com/v3.1/name/';
+const filter ='fields=name,capital,population,flags,languages'
+
+export function fetchCountries(name) {
+  return fetch(`${url}${name}?${filter}`).then(response => {
+        if (!response.ok) {
+            throw Notify.failure("Oops, there is no country with that name");
+        }
+        return response.json();
+    });
+    };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,51 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
+
 import { fetchCountries } from './fetchCountries';
+import { renderListCountries } from './rendering';
+import { rendercountry } from './rendering';
 
 const DEBOUNCE_DELAY = 300;
 
-const inputEl = document.getElementById('search-box');
-const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
-
-inputEl.addEventListener('input', (event) => {
-   const inputTerm = event.targer.value.trim();
-
-if( inputTerm === '') {
-    countryList.innerHTML = '';
-    countryInfo.innerHTML = '';
-    return;
+const refs = {
+    inputEl: document.querySelector('#search-box'),
+    countryLlist: document.querySelector('.country-list'),
+    countryIinfo: document.querySelector('.country-info'),
 }
 
-});
+refs.inputEl.addEventListener("input", debounce(onFetchCountries, DEBOUNCE_DELAY));
+
+function onFetchCountries(event) {
+    const inputValue = event.target.value.trim();
+    if (!inputValue) {
+     clearHtml();   
+        return;
+    }
+    fetchCountries(inputValue)
+        .then(arrayCoutries => { 
+            if (arrayCoutries.length > 10) {
+                Notify.info("Too many matches found. Please enter a more specific name.");
+                clearHtml();
+                return;  
+            }
+            if (arrayCoutries.length > 1) {
+                refs.countryIinfo.innerHTML = "";
+                refs.countryLlist.innerHTML = renderListCountries(arrayCoutries);
+                return;
+            }
+            refs.countryLlist.innerHTML = "";
+            refs.countryIinfo.innerHTML = rendercountry(arrayCoutries);
+        })
+        .catch(error => {
+            clearHtml();
+          })
+}
+
+function clearHtml() {
+     refs.countryLlist.innerHTML = "";
+     refs.countryIinfo.innerHTML = "";  
+}
 
 
 
